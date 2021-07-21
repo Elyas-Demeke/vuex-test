9fc027b75c8f3fcb8311977ec5db542791d93e2f
Elyas demeke <elyas@zeraftech.com>
2021-07-21 10:16:42 +0300

    Background color and about us changed

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <p>ads</p>
+       <h1>About Us</h1>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet, officiis minus. Sed voluptatem totam magni consectetur delectus, nisi saepe doloribus ut. Libero iure rerum excepturi rem corporis aliquid? Earum, aut?</p>
     </div>
 </template>
 
